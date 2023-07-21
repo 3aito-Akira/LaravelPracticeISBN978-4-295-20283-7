41f47aac9e3140069e15fb7a17cbaf1ef79ad911
akira <yuhstamtch@yahoo.co.jp>
2023-07-21 14:57:59 +0900

    second commit

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -1,50 +1,9 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width ,user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>つぶやきアプリ</title>
-</head>
-<body>
-    <h1>つぶやきアプリ</h1>
-@auth
-    <div>
-        <p>input form</p>
-        @if(session('feedback.success'))
-        <p style="color: green">{{ session('feedback.success') }}</p>
-        @endif
-        <form action="{{ route('tweet.create') }}" method="post">
-            @csrf
-            <label for="tweet-content">tweet</label>
-            <span>within 140 letters</span>
-            <textarea id="tweet-content" type="text" name="tweet" placeholder="input message"></textarea>
-            @error('tweet')
-            <p style="color: red;">{{ $message }}</p>
-            @enderror
-            <button type="submit">submit</button>
-        </form>
-    </div>
-@endauth
-<div>
-    @foreach($tweets as $tweet)
-        <details>
-            <summary>{{ $tweet->content }} by {{ $tweet->user->name }}</summary>
-            @if(\Illuminate\Support\Facades\Auth::id() === $tweet->user_id)
-            <div>
-                <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">update</a>
-                <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
-                    @method('DELETE')
-                    @csrf
-                    <button type="submit">delete</button>
-                </form>
-            </div>
-            @else
-                You can not update this tweet.
-            @endif
-        </details>
-    @endforeach
-</div>
-</body>
-</html>
+<x-layout title="TOP | Tweet Application">
+    <x-layout.single>
+        <h2 class="text-center text-blue-500 text-4xl font-bold mt-8 mb-8">
+            Tweet Application
+        </h2>
+        <x-tweet.form.post></x-tweet.form.post>
+        <x-tweet.list :tweets="$tweets"></x-tweet.list>
+    </x-layout.single>
+</x-layout>
